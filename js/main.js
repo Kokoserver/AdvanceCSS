2dd43fc366cff2ef8163ed02e07ee605b460a5d2
owonikoko olaoluwa <owonikokoolaoluwa@gmail.com>
2020-07-28 01:43:32 +0100

    working with navbar

--- js/main.js
+++ js/main.js
@@ -1,20 +1,20 @@
-const sections = document.querySelectorAll("section");
-const options = {
-  root: null, // means you are refaring to current viewport, or you can leave it
-  threshold:0.25, //ranging from 0 to 1
-  rootMargin:"300px"
-}
-const observer = new IntersectionObserver((entries, observer)=>{
-  entries.forEach((entry) => {
-    if(!entry.isIntersecting){
-      return
-    }
-
-    console.log(entry.target);
-    observer.unobserve(entry.target)
-  });
-}, options)
-
-sections.forEach((section) => {
-  observer.observe(section)
-});
+const sections = document.querySelector(".navbar__link");
+// const options = {
+//   root: null, // means you are refaring to current viewport, or you can leave it
+//   threshold:0.25, //ranging from 0 to 1
+//   rootMargin:"300px"
+// }
+// const observer = new IntersectionObserver((entries, observer)=>{
+//   entries.forEach((entry) => {
+//     if(!entry.isIntersecting){
+//       return
+//     }
+//
+//     console.log(entry.target);
+//     observer.unobserve(entry.target)
+//   });
+// }, options)
+//
+// sections.forEach((section) => {
+//   observer.observe(section)
+// });
